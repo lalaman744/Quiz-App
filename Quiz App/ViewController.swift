764f8325e27f41227bb06170d1dbda1f0861d5e7
Josh Kleinschmidt <lalaman744@hotmail.com>
2019-01-21 10:12:17 -0600

    quiz app 2 labels, 2 buttons - 3 questions/answers

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  Quiz App
-//
-//  Created by Josh Kleinschmidt on 1/20/19.
-//  Copyright © 2019 Josh Kleinschmidt. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
 }
 
